179f2917a90508381e82f9110a2283b1948796b7
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-01 14:22:40 +0200

    add header and sidebar components

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router'
+
+function Header() {
+
+    const { userId } = useDataContext();
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <header className="header">
+            <img className="header_img" src={Logo} alt="SportSee - logo" />
+            <img className="header_hamb" src={Hamb} alt="SportSee - menu" onClick={() => setToggleMenu(!toggleMenu)} />
+            <nav className={toggleMenu ? "header_nav visible" : "header_nav"}>
+                <ul>
+                    <Link to="/">
+                        <li>Accueil</li>
+                    </Link>
+                    <Link to={userId && `/profile/${userId}`}>
+                        <li>Profil</li>
+                    </Link>
+                    <Link to="/">
+                        <li>Réglage</li>
+                    </Link>
+                    <Link to="/">
+                        <li>Communauté</li>
+                    </Link>
+                </ul>
+            </nav>
+        </header>
+  )
+}
+
+export default Header
