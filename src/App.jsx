76ff1fce4ff3416264ecd92be37c77d2803260f0
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-06 18:16:22 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Error from './pages/Error'
+import Settings from './pages/Settings'
+import Community from './pages/Community'
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user/:userId" element={<Dashboard />} />
-          <Route path="/*" element={<Error />} />
-        </Routes>
-      </Router>
+      <BrowserRouter>
+				<Header />
+				<Sidebar />
+        <main>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/user/:userId" element={<Dashboard />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/community" element={<Community />} />
+          			<Route path="*" element={<Error />} />
+				</Routes>
+        </main>
+			</BrowserRouter>
     </div>
   )
 }
