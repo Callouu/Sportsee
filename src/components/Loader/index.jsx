e3cc68f50ea3d6822989c7fe8ad0ede19f59b36d
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-09 10:44:16 +0200

    add styles

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * @component
+ * @description Component Loader who appears when the datas are not fetched yet
+ */
+function Loader() {
+	return (
+		<div className="loader">Chargement...</div>
+	)
+}
+
+Loader.propTypes = {
+
+}
+
+export default Loader;
