146e87584d36f2f6c1d41dadf59533fbceb09ceb
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-13 21:28:12 +0200

    add styles to sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -14,13 +14,13 @@ import haltere from '../../assets/haltere.svg'
 function Sidebar() {
 	return (
 		<aside className="sidebar">
-			<div className="sidebar-div">
+			<div className="sidebar-button-wrapper center">
 				<SidebarIcon logo={yoga} />
 				<SidebarIcon logo={swimming} />
 				<SidebarIcon logo={biking} />
 				<SidebarIcon logo={haltere} />
 			</div>
-			<p className="sidebar-text">Copyright, SportSee 2020</p>
+			<p className="copyright">Copyright, SportSee 2020</p>
 		</aside>
 	)
 }
