5f65c76be8ebcefe8a24b2172e54b89e78aa99d3
Alexis Bouvry <bouvryalexis@gmail.com>
2025-06-16 14:34:23 +0200

    change vite config

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import './style.css'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Sportsee">
 				<Header />
 				<Sidebar />
         <main>
